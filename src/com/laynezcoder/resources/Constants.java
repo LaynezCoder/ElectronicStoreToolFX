86f44ab205903575b45c8e60a20f15c72e99cfa7
LaynezCoder <vlaynez2016@gmail.com>
2021-01-04 19:51:58 -0600

    The Constants class has been modified.

--- src/com/laynezcoder/resources/Constants.java
+++ src/com/laynezcoder/resources/Constants.java
@@ -2,5 +2,5 @@ package com.laynezcoder.resources;
 
 public class Constants {
     public static final String RIMOUSKI_FONT = "/com/laynezcoder/fonts/rimouski.ttf";
-    public static final String PROFILE_PICTURES_PACKAGE = "/com/laynezcoder/media/profilepictures/";
+    public static final String PROFILE_PICTURES_PACKAGE = "/com/laynezcoder/media/profile/images/";
 }
