c30f12190520007c6eaa60de896f77fa67cc27ed
LaynezCoder <vlaynez2016@gmail.com>
2021-01-04 19:43:06 -0600

    A class has been created to crop profile images.

---
+++ src/com/laynezcoder/util/CropImageProfile.java
@@ -0,0 +1,116 @@
+package com.laynezcoder.util;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Bounds;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javax.imageio.ImageIO;
+
+public class CropImageProfile {
+
+    private ImageView imageView;
+
+    private double imageHeight;
+
+    private double imageWidth;
+
+    private final File file;
+
+    private InputStream outputStream;
+
+    private InputStream inputStream;
+
+    private Rectangle rect = new Rectangle();
+
+    private Group group;
+
+    public CropImageProfile(File file) {
+        this.file = file;
+        createWorkSpace();
+    }
+
+    private void createWorkSpace() {
+        group = new Group();
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CropImageProfile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Image image = new Image(inputStream);
+        imageHeight = image.getHeight();
+        imageWidth = image.getWidth();
+
+        imageView = new ImageView(image);
+        imageView.setFitHeight(imageHeight);
+        imageView.setFitWidth(imageWidth);
+        imageView.setPreserveRatio(true);
+
+        rect = new Rectangle();
+        group.getChildren().addAll(imageView, rect);
+
+        if (imageWidth > imageHeight) {
+            rect.setWidth(imageHeight);
+            rect.setHeight(imageHeight);
+            rect.setX(imageWidth / 2 - imageHeight / 2);
+            rect.setY(0);
+        } else if (imageHeight > imageWidth) {
+            rect.setWidth(imageWidth);
+            rect.setHeight(imageWidth);
+            rect.setX(0);
+            rect.setY(imageHeight / 2 - imageWidth / 2);
+        } else if (imageHeight == imageWidth) {
+            group.getChildren().remove(rect);
+        }
+    }
+
+    public InputStream getInputStream() throws FileNotFoundException {
+        if (!group.getChildren().contains(rect)) {
+            return inputStream = new FileInputStream(file);
+        } else {
+            Bounds bounds = rect.getBoundsInParent();
+            int width = (int) bounds.getWidth();
+            int height = (int) bounds.getHeight();
+
+            SnapshotParameters parameters = new SnapshotParameters();
+            parameters.setFill(Color.TRANSPARENT);
+            parameters.setViewport(new Rectangle2D(bounds.getMinX(), bounds.getMinY(), width, height));
+
+            WritableImage wi = new WritableImage(width, height);
+            imageView.snapshot(parameters, wi);
+
+            BufferedImage bufImageARGB = SwingFXUtils.fromFXImage(wi, null);
+            BufferedImage bufImageRGB = new BufferedImage(bufImageARGB.getWidth(), bufImageARGB.getHeight(), BufferedImage.OPAQUE);
+
+            Graphics2D graphics = bufImageRGB.createGraphics();
+            graphics.drawImage(bufImageARGB, 0, 0, null);
+
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            try {
+                ImageIO.write(bufImageRGB, "jpg", os);
+            } catch (IOException ex) {
+                Logger.getLogger(CropImageProfile.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            outputStream = new ByteArrayInputStream(os.toByteArray());
+            graphics.dispose();
+        }
+        return outputStream;
+    }
+}
