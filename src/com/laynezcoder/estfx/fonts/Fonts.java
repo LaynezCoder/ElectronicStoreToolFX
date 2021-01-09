61eb94ebd013cb2fd673662168355008597952a0
LaynezCoder <vlaynez2016@gmail.com>
2021-01-08 23:49:10 -0600

    The Fonts class has been modified.

--- src/com/laynezcoder/estfx/fonts/Fonts.java
+++ src/com/laynezcoder/estfx/fonts/Fonts.java
@@ -1,20 +1,19 @@
 package com.laynezcoder.estfx.fonts;
 
+import com.laynezcoder.estfx.resources.Constants;
 import javafx.scene.control.Button;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class Fonts {
 
-    public static final String RIMOUSKI_FONT = "/com/laynezcoder/estfx/fonts/rimouski.ttf";
-
     public static void toText(Text text, int sizeFont) {
-        Font font = Font.loadFont(Fonts.class.getResourceAsStream(RIMOUSKI_FONT), sizeFont);
+        Font font = Font.loadFont(Constants.RIMOUSKI_FONT, sizeFont);
         text.setFont(font);
     }
 
     public static void toButton(Button btn, int sizeFont) {
-        Font font = Font.loadFont(Fonts.class.getResourceAsStream(RIMOUSKI_FONT), sizeFont);
+        Font font = Font.loadFont(Constants.RIMOUSKI_FONT, sizeFont);
         btn.setFont(font);
     }
 }
