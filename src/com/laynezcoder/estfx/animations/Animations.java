c2732dfa78e69d8ea50ce95cad00b81101b8d11b
LaynezCoder <vlaynez2016@gmail.com>
2021-01-08 22:00:27 -0600

    Some methods have been added

--- src/com/laynezcoder/estfx/animations/Animations.java
+++ src/com/laynezcoder/estfx/animations/Animations.java
@@ -1,8 +1,11 @@
 package com.laynezcoder.estfx.animations;
 
+import animatefx.animation.FadeIn;
 import animatefx.animation.FadeInUp;
 import animatefx.animation.Shake;
+import javafx.animation.ScaleTransition;
 import javafx.scene.Node;
+import javafx.util.Duration;
 
 public class Animations {
 
@@ -12,5 +15,36 @@ public class Animations {
 
     public static void shake(Node node) {
         new Shake(node).play();
+    }
+
+    public static void tooltip(Node node, Node tooltip) {
+        node.setOnMouseEntered(ev -> {
+            FadeIn fadeIn = new FadeIn(tooltip);
+            fadeIn.setSpeed(3);
+            fadeIn.play();
+            tooltip.setVisible(true);
+        });
+
+        node.setOnMouseExited(ev -> {
+            tooltip.setVisible(false);
+        });
+    }
+
+    public static void hover(Node node, int duration, double setXAndY) {
+        ScaleTransition scaleTrans = new ScaleTransition(Duration.millis(duration), node);
+        scaleTrans.setFromX(1.0);
+        scaleTrans.setFromY(1.0);
+        scaleTrans.setToX(setXAndY);
+        scaleTrans.setToY(setXAndY);
+
+        node.setOnMouseEntered(ev -> {
+            scaleTrans.setRate(1.0);
+            scaleTrans.play();
+        });
+
+        node.setOnMouseExited(ev -> {
+            scaleTrans.setRate(-1.0);
+            scaleTrans.play();
+        });
     }
 }
