0cb2079a91e6ef77bc1029671191ede63c141d55
LaynezCoder <vlaynez2016@gmail.com>
2021-01-08 21:34:31 -0600

    The JFXDialogTool class has been created.

---
+++ src/com/laynezcoder/util/JFXDialogTool.java
@@ -0,0 +1,45 @@
+package com.laynezcoder.util;
+
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.events.JFXDialogEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import com.laynezcoder.resources.Constants;
+import com.laynezcoder.database.DatabaseHelper;
+import javafx.event.EventHandler;
+
+public class JFXDialogTool {
+
+    private final JFXDialog dialog;
+
+    public JFXDialogTool(Region region, StackPane container) {
+        dialog = new JFXDialog();
+        dialog.setContent(region);
+        dialog.setBackground(Background.EMPTY);
+        dialog.setDialogContainer(container);
+        dialog.setTransitionType(DatabaseHelper.dialogTransition());
+        setStyle();
+    }
+
+    private void setStyle() {
+        dialog.getStylesheets().add(Constants.LIGHT_THEME);
+        dialog.getStyleClass().add("jfx-dialog-overlay-pane");
+    }
+
+    public void setOnDialogOpened(EventHandler<JFXDialogEvent> action) {
+        dialog.setOnDialogOpened(action);
+    }
+
+    public void setOnDialogClosed(EventHandler<JFXDialogEvent> action) {
+        dialog.setOnDialogClosed(action);
+    }
+
+    public void show() {
+        dialog.show();
+    }
+
+    public void close() {
+        dialog.close();
+    }
+}
