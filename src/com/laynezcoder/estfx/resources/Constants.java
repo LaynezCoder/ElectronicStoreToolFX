07fadeccdc80cc3683df4141edb8d05602186119
LaynezCoder <vlaynez2016@gmail.com>
2021-01-08 21:41:05 -0600

    The Constans class has been modified.

--- src/com/laynezcoder/estfx/resources/Constants.java
+++ src/com/laynezcoder/estfx/resources/Constants.java
@@ -3,8 +3,7 @@ package com.laynezcoder.estfx.resources;
 public class Constants {
 
     public static final String MEDIA_PACKAGE = "/com/laynezcoder/estfx/media/";
-    public static final String CSS_LIGHT_THEME = "/com/laynezcoder/resources/LightTheme.css";
+    public static final String CSS_LIGHT_THEME = "/com/laynezcoder/estfx/resources/LightTheme.css";
     public static final String PROFILE_PICTURES_PACKAGE = "/com/laynezcoder/media/profile/images/";
-    public static final String ICON = "/com/laynezcoder/media/profile/images/";
     public static final String LIGHT_THEME = Constants.class.getResource(CSS_LIGHT_THEME).toExternalForm();
 }
