2e1bccd0a23b5b4908f038b1b5d3091e72f784b3
LaynezCoder <vlaynez2016@gmail.com>
2021-01-08 21:56:13 -0600

    The RequieredFieldsValidatios and TextField Mask classes have changed packages.

--- src/com/laynezcoder/estfx/mask/RequieredFieldsValidators.java
+++ src/com/laynezcoder/estfx/mask/RequieredFieldsValidators.java
@@ -1,4 +1,4 @@
-package com.laynezcoder.util;
+package com.laynezcoder.estfx.mask;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXPasswordField;
--- src/com/laynezcoder/estfx/mask/TextFieldMask.java
+++ src/com/laynezcoder/estfx/mask/TextFieldMask.java
@@ -1,4 +1,4 @@
-package com.laynezcoder.util;
+package com.laynezcoder.estfx.mask;
 
 import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
