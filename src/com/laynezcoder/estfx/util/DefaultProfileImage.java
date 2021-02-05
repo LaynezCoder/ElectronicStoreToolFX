08a489a9376e9bb4b31610c2933dfac550800285
LaynezCoder <vlaynez2016@gmail.com>
2021-02-05 16:24:38 -0600

    Readability: The image assignment of a switch statement to a for loop has been changed.

--- src/com/laynezcoder/estfx/util/DefaultProfileImage.java
+++ src/com/laynezcoder/estfx/util/DefaultProfileImage.java
@@ -22,92 +22,17 @@ import java.io.InputStream;
 
 public class DefaultProfileImage {
 
+    private static final String ABC = "abcdefghijklmnopqrstwxyz";
+
     public static InputStream getImage(String name) throws FileNotFoundException {
         InputStream inputStream = null;
 
         char character = name.toLowerCase().charAt(0);
-        switch (character) {
-            case 'a':
-                inputStream = getProfilePictureFromPackage("a");
-                break;
-            case 'b':
-                inputStream = getProfilePictureFromPackage("b");
-                break;
-            case 'c':
-                inputStream = getProfilePictureFromPackage("c");
-                break;
-            case 'd':
-                inputStream = getProfilePictureFromPackage("d");
-                break;
-            case 'e':
-                inputStream = getProfilePictureFromPackage("e");
-                break;
-            case 'f':
-                inputStream = getProfilePictureFromPackage("f");
-                break;
-            case 'g':
-                inputStream = getProfilePictureFromPackage("g");
-                break;
-            case 'h':
-                inputStream = getProfilePictureFromPackage("h");
-                break;
-            case 'i':
-                inputStream = getProfilePictureFromPackage("i");
-                break;
-            case 'j':
-                inputStream = getProfilePictureFromPackage("j");
-                break;
-            case 'k':
-                inputStream = getProfilePictureFromPackage("k");
-                break;
-            case 'l':
-                inputStream = getProfilePictureFromPackage("l");
-                break;
-            case 'm':
-                inputStream = getProfilePictureFromPackage("m");
-                break;
-            case 'n':
-                inputStream = getProfilePictureFromPackage("n");
-                break;
-            case 'ñ':
-                inputStream = getProfilePictureFromPackage("n");
-                break;
-            case 'o':
-                inputStream = getProfilePictureFromPackage("o");
-                break;
-            case 'p':
-                inputStream = getProfilePictureFromPackage("p");
-                break;
-            case 'q':
-                inputStream = getProfilePictureFromPackage("q");
-                break;
-            case 'r':
-                inputStream = getProfilePictureFromPackage("r");
-                break;
-            case 's':
-                inputStream = getProfilePictureFromPackage("s");
-                break;
-            case 't':
-                inputStream = getProfilePictureFromPackage("t");
-                break;
-            case 'u':
-                inputStream = getProfilePictureFromPackage("u");
-                break;
-            case 'v':
-                inputStream = getProfilePictureFromPackage("v");
-                break;
-            case 'w':
-                inputStream = getProfilePictureFromPackage("w");
-                break;
-            case 'x':
-                inputStream = getProfilePictureFromPackage("x");
-                break;
-            case 'y':
-                inputStream = getProfilePictureFromPackage("y");
-                break;
-            case 'z':
-                inputStream = getProfilePictureFromPackage("z");
-                break;
+
+        for (int i = 0; i < ABC.length(); i++) {
+            if (ABC.charAt(i) == character) {
+                inputStream = getProfilePictureFromPackage(String.valueOf(ABC.charAt(i)));
+            }
         }
         return inputStream;
     }
