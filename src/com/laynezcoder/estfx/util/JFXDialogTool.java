1dbc6455fb48818e498ddc87343c60e31248b0cf
LaynezCoder <vlaynez2016@gmail.com>
2021-03-20 23:56:06 -0600

    Modified class: JFXDialogTool

--- src/com/laynezcoder/estfx/util/JFXDialogTool.java
+++ src/com/laynezcoder/estfx/util/JFXDialogTool.java
@@ -16,38 +16,18 @@
 package com.laynezcoder.estfx.util;
 
 import com.jfoenix.controls.JFXDialog;
-import com.jfoenix.controls.events.JFXDialogEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import com.laynezcoder.estfx.database.DatabaseHelper;
-import javafx.event.EventHandler;
 
-public class JFXDialogTool {
-
-    private final JFXDialog dialog;
+public class JFXDialogTool extends JFXDialog {
 
     public JFXDialogTool(Region region, StackPane container) {
-        dialog = new JFXDialog();
-        dialog.setContent(region);
-        dialog.setBackground(Background.EMPTY);
-        dialog.setDialogContainer(container);
-        dialog.getStyleClass().add("jfx-dialog-overlay-pane");
-        dialog.setTransitionType(DatabaseHelper.dialogTransition()); 
-    }
-    public void setOnDialogOpened(EventHandler<JFXDialogEvent> action) {
-        dialog.setOnDialogOpened(action);
-    }
-
-    public void setOnDialogClosed(EventHandler<JFXDialogEvent> action) {
-        dialog.setOnDialogClosed(action);
-    }
-
-    public void show() {
-        dialog.show();
-    }
-
-    public void close() {
-        dialog.close();
+        setContent(region);
+        setBackground(Background.EMPTY);
+        setDialogContainer(container);
+        getStyleClass().add("jfx-dialog-overlay-pane");
+        setTransitionType(DatabaseHelper.dialogTransition()); 
     }
 }
