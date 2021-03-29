ed73636db708d060e6257ae656c8e4cae3deaa7d
LaynezCoder <vlaynez2016@gmail.com>
2021-03-28 23:10:18 -0600

    Modified class: I18NUtil

--- src/com/laynezcoder/estfx/util/I18nUtil.java
+++ src/com/laynezcoder/estfx/util/I18nUtil.java
@@ -26,7 +26,7 @@ import javafx.scene.Parent;
 public class I18NUtil {
 
     private static final String BUNDLE = "resources.com.laynezcoder.i18n.gui";
-   
+    
     public static Parent loadView(Views nameView) throws IOException {
         Locale.setDefault(Locale.getDefault());
 
