bf648efb6e8d971b3676c320b124e22499decdc7
LaynezCoder <vlaynez2016@gmail.com>
2021-03-28 20:16:45 -0600

    Added class: messages

---
+++ src/com/laynezcoder/estfx/resources/Messages.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020-2021 LaynezCode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.laynezcoder.estfx.resources;
+
+public final class Messages {
+
+    private Messages() {
+
+    }
+
+    public static final String ERROR_CONNECTION_MYSQL = "An error occurred when connecting to MySQL.\nCheck your connection to MySQL";
+
+    public static final String IMAGE_LARGE = "Please upload a picture smaller than 1 MB.";
+
+    public static final String IMAGE_NOT_FOUND = "Image not found. The record will be saved.";
+
+    public static final String INSUFFICIENT_DATA = "Insufficient data";
+
+    public static final String USER_ALREADY_EXISTS = "This user already exists";
+
+    public static final String PASSWORDS_NOT_MATCH = "Passwords do not match.";
+
+    public static final String ENTER_AT_LEAST_4_CHARACTERES = "Please enter at least 4 characters";
+
+    public static final String NO_RECORD_SELECTED = "Select an item from the table.";
+
+    public static final String ADDED_RECORD = "Record added successfully";
+
+    public static final String UPDATED_RECORD = "Record updated successfully";
+
+    public static final String DELETED_RECORD = "Record deleted successfully";
+
+    public static final String EXCEPTION = "Something strange happend";
+}
