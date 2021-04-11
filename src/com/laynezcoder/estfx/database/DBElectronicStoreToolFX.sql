a393f7b7459cee936390628eaf29e2491317a34f
LaynezCoder <vlaynez2016@gmail.com>
2021-04-11 00:02:54 -0600

    Modified DB: database

--- src/com/laynezcoder/estfx/database/DBElectronicStoreToolFX.sql
+++ src/com/laynezcoder/estfx/database/DBElectronicStoreToolFX.sql
@@ -17,14 +17,33 @@ DROP DATABASE IF EXISTS DBElectronicStoreToolFX;
 CREATE DATABASE DBElectronicStoreToolFX;
 USE DBElectronicStoreToolFX;
 
+CREATE TABLE Users (
+	id INT NOT NULL AUTO_INCREMENT,
+    fullname VARCHAR(150) NOT NULL,
+	username VARCHAR(150) NOT NULL,
+	pass VARCHAR(150) NOT NULL,
+    biography LONGTEXT NOT NULL,
+    dialogTransition VARCHAR(25) NOT NULL,
+    isActive BOOLEAN NOT NULL,
+    userType VARCHAR(150) NOT NULL,
+    profileImage LONGBLOB NOT NULL,
+    linkProfile VARCHAR(150) NOT NULL,
+    insertionDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE Customers (
 	id INT NOT NULL AUTO_INCREMENT,
     customerName VARCHAR(150) NOT NULL,
     customerNumber VARCHAR(50) NOT NULL,
     customerEmail VARCHAR(150) NOT NULL,
     it VARCHAR(50) NOT NULL,
-    insertionDate DATE NOT NULL,
-    PRIMARY KEY (id)
+    insertionDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    userId INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT FOREIGN KEY (userId)
+    REFERENCES Users (id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE Quotes (
@@ -35,10 +54,15 @@ CREATE TABLE Quotes (
     existence VARCHAR(20) NOT NULL,
     realization VARCHAR(20) NOT NULL,
     report VARCHAR (20) NOT NULL,
+    insertionDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     customerId INT NOT NULL,
+    userId INT NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT FOREIGN KEY (customerId)
     REFERENCES Customers (id)
+    ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (userId)
+    REFERENCES Users (id)
     ON DELETE CASCADE
 );
 
@@ -52,38 +76,22 @@ CREATE TABLE Products (
     minimalPrice DOUBLE NOT NULL,
     descriptionProduct LONGTEXT NOT NULL,
     imageProduct MEDIUMBLOB NOT NULL,
-    insertionDate DATE NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE Users (
-	id INT NOT NULL AUTO_INCREMENT,
-    nameUser VARCHAR(150) NOT NULL,
-	email VARCHAR(150) NOT NULL,
-	pass VARCHAR(150) NOT NULL,
-    biography LONGTEXT NOT NULL,
-    dialogTransition VARCHAR(150) NOT NULL,
-    userType VARCHAR(150) NOT NULL,
-    profileImage LONGBLOB NOT NULL,
-    PRIMARY KEY (id)
+    insertionDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    userId INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT FOREIGN KEY (userId)
+    REFERENCES Users (id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE UserSession (
 	id INT NOT NULL AUTO_INCREMENT,
     userId INT NOT NULL,
+    lastUpdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     CONSTRAINT FOREIGN KEY (userId)
     REFERENCES Users (id)
 );
 
-CREATE TRIGGER triggerAddCustomerInsertDate BEFORE INSERT ON Customers	
-    FOR EACH ROW SET NEW.insertionDate = NOW();
-    
-CREATE TRIGGER triggerAddProductInsertDate BEFORE INSERT ON Products	
-	FOR EACH ROW SET NEW.insertionDate = NOW();
-
-CREATE TRIGGER triggerAddDialogTransition BEFORE INSERT ON Users	
-    FOR EACH ROW SET NEW.dialogTransition = "CENTER";
-    
-CREATE TRIGGER triggerAddBiography BEFORE INSERT ON Users	
-    FOR EACH ROW SET NEW.biography = "Every day is a new opportunity to change your life.";
+CREATE TRIGGER triggerUsers BEFORE INSERT ON Users	
+    FOR EACH ROW SET NEW.biography = "Every day is a new opportunity to change your life.", NEW.dialogTransition = "CENTER", NEW.isActive = true;
