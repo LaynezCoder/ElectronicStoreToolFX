b80257b91e7349eb61cfbb8b06e056889b77bb01
LaynezCoder <vlaynez2016@gmail.com>
2021-04-12 13:53:16 -0600

    Modified files: Internationalization has been added

--- src/com/laynezcoder/estfx/controllers/TestController.java
+++ src/com/laynezcoder/estfx/controllers/TestController.java
@@ -40,8 +40,12 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -180,6 +184,30 @@ public class TestController implements Initializable {
 
     @FXML
     private Button btnValuableUser;
+    
+     @FXML
+    private VBox containerAdd;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private PasswordField txtConfirmPassword;
+
+    @FXML
+    private ComboBox<String> cmbDialogTransition;
+
+    @FXML
+    private TextArea txtDescription;
+
+    @FXML
+    private HBox buttonsContainer;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
